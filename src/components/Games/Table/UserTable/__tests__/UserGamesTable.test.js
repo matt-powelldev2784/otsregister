f7c017eebddc0ba6541fb8b209595896b47cfcf9
Matthew Powell <matt.powell2784@gmail.com>
2022-09-16 18:09:58 +0100

    tests updated due to apil util updated

--- src/components/Games/Table/UserTable/__tests__/UserGamesTable.test.js
+++ src/components/Games/Table/UserTable/__tests__/UserGamesTable.test.js
@@ -10,7 +10,7 @@ import { apiCall } from '../../../../Utilities/apiUtil'
 
 describe('When the api call is sucessful', () => {
     it('should render 4 rows in the table', async () => {
-        await apiCall({ apiCallType: 'get', route: 'api/games/recentgames', token: 'dummyAuthToken' })
+        await apiCall({ apiCallType: 'GET', route: 'api/games/recentgames', token: 'dummyAuthToken' })
 
         render(
             <Provider store={apiSucessStore}>
@@ -25,7 +25,7 @@ describe('When the api call is sucessful', () => {
 
 describe('When the api call fails', () => {
     it('should render 4 rows in the table', async () => {
-        await apiCall({ apiCallType: 'get', route: 'api/games/recentgames', token: 'dummyAuthToken' })
+        await apiCall({ apiCallType: 'GET', route: 'api/games/recentgames', token: 'dummyAuthToken' })
 
         render(
             <Provider store={apiFailStore}>
