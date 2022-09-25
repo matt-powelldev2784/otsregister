eb4baeb95de72cb0c00582b0e90563d03d3c459a
Matthew Powell <matt.powell2784@gmail.com>
2022-09-25 08:16:15 +0100

    typescript types added

--- src/Pages/EditProfilePage.js
+++ src/Pages/EditProfilePage.js
@@ -1,32 +1,31 @@
-import React, { Fragment, useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { getAuhtorisedUser } from '../components/redux/authState';
-import { setMenu } from '../components/redux/globalState';
-import styled from 'styled-components';
-import { Header } from '../components/Header/Header';
-import { Background } from '../components/Header/Background';
-import { Profile } from '../components/Profile/Profile';
-import { Errors } from '../components/Utilities/Errors';
+import React, { Fragment, useEffect } from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { setMenu } from '../components/redux/globalState'
+import styled from 'styled-components'
+import { Header } from '../components/Header/Header'
+import { Background } from '../components/Header/Background'
+import { Profile } from '../components/Profile/Profile'
+import { Errors } from '../components/Utilities/Errors'
 
 export const EditProfilePage = () => {
-    const dispatch = useDispatch();
-    const { isDesktop } = useSelector(state => state.globalReducer);
-    const { authToken, adminUser, authErrors } = useSelector(state => state.authReducer);
-    const { dataErrors } = useSelector(state => state.dataReducer);
+    const dispatch = useDispatch()
+    const { isDesktop } = useSelector(state => state.globalReducer)
+    const { authToken, adminUser, authErrors } = useSelector(state => state.authReducer)
+    const { dataErrors } = useSelector(state => state.dataReducer)
 
     useEffect(() => {
         try {
             if (authToken) {
-                dispatch(getAuhtorisedUser(authToken));
+                dispatch(getAuhtorisedUser(authToken))
             }
         } catch (err) {
-            throw Error;
+            throw Error
         }
-    }, [authToken, dispatch]);
+    }, [authToken, dispatch])
 
     useEffect(() => {
-        dispatch(setMenu({ homepage: false, adminUser: adminUser, user: !adminUser }));
-    }, [dispatch, adminUser]);
+        dispatch(setMenu({ homepage: false, adminUser: adminUser, user: !adminUser }))
+    }, [dispatch, adminUser])
 
     return (
         <Fragment>
@@ -40,15 +39,15 @@ export const EditProfilePage = () => {
                 </Container>
             </Main>
         </Fragment>
-    );
-};
+    )
+}
 
 const Main = styled.main`
     position: relative;
-`;
+`
 
 const Container = styled.section`
     position: absolute;
     top: 0;
     width: 100%;
-`;
+`
